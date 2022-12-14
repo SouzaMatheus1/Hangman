1bffd6709387af8ba84dff070b85280f99dc5e4d
Matheus Souza <matheus3864@gmail.com>
2022-12-14 13:15:32 -0300

    Update ini.sh

--- ini.sh
+++ ini.sh
@@ -1,8 +1,34 @@
-sudo apt install  python3-pip
-pip install random
-pip install pandas
-pip install os
+BIBLIOTECA_PANDAS=$(pip freeze | grep pandas)
+BIBLIOTECA_RANDOM=$(pip freeze | grep random)
+APT_PIP=$(sudo apt list | grep python3-pip)
 
-echo "Random e Pandas ok"
+clear
+if [ "" = "$APT_PIP" ]; then
+    sudo apt install python3-pip
+
+  echo '\n-------------------------------------------------------'
+  echo 'pip instalado com sucesso.'
+fi
+
+if [ "" = "$BIBLIOTECA_PANDAS" ]; then
+  pip install pandas
+
+  echo '\n-------------------------------------------------------'
+  echo 'Biblioteca Pandas instalado com sucesso.'
+else
+  echo "Biblioteca Pandas já instalado."
+fi
+
+if [ "" = "$BIBLIOTECA_RANDOM" ]; then
+  pip install pandas
+
+  echo '\n-------------------------------------------------------'
+  echo 'Biblioteca Random instalado com sucesso.'
+else
+  echo "Biblioteca Pandas já instalado."
+fi
+
+echo '\n-------------------------------------------------------'
+echo 'Iniciando jogo...'
 
 python3 Hangman.py
